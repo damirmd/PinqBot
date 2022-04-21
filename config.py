75d5026fa3e1ad1740bd80a2487103d60f4bdd06
Pinqfox <morgen@shtern.ga>
2022-04-21 08:24:54 +0300

    Убрал токен из открытого доступа

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'OTY0NTA2MjAzMTY0MTE5MDUw.YlloZg.KU7zPapLJh8FwZTGSRDL6DBymN8',
+    'token': '',
     'bot': 'PinqWeather',
     'id': 964506203164119050,
     'prefix': '!',
